fb1bd1d185f1c60473e283e7b75404fd566cf50e
Prabhleen-11 <prabhukaur011@gmail.com>
2025-11-10 23:43:57 +0530

    Frontend of Community Section done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
 import LandingPage from './sections/LandingPage'
-
+import Posts from './sections/posts.jsx'
 function App() {
   
 
   return (
     <>
       <LandingPage></LandingPage>
+      <Posts></Posts>
     </>
   )
 }
